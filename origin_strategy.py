7094653b95ef9d1b7453e46888d6dad00b10047c
hurocup_white <humanoidgithub@gmail.com>
2023-07-04 09:29:23 +0800

    head_move problem

--- origin_strategy.py
+++ origin_strategy.py
@@ -6,7 +6,7 @@ import cv2
 from Python_API import Sendmessage
 import time
 
-HEAD_Y_HIGH = 1700
+HEAD_Y_HIGH = 1600
 HEAD_Y_LOW = 1375
 HEAD_X_MAX = 2700
 HEAD_X_MIN = 1400
@@ -33,7 +33,7 @@ class United_Soccer:
                 self.head_status = "High"
             rospy.logdebug(f'低頭低頭低頭低頭低頭低頭低頭低頭低頭低頭低頭低頭低頭低頭低頭低頭')
             send.sendHeadMotor(1, self.head_x, 100)
-            send.sendHeadMotor(2, HEAD_Y_LOW, 50)
+            send.sendHeadMotor(2, HEAD_Y_HIGH, 50)
             time.sleep(0.01)
         elif self.head_status == "High":
             self.head_x = min(self.head_x, HEAD_X_MAX)
@@ -42,7 +42,7 @@ class United_Soccer:
                 self.head_status = "Low"
             rospy.logdebug(f'抬頭抬頭抬頭抬頭抬頭抬頭抬頭抬頭抬頭抬')
             send.sendHeadMotor(1, self.head_x, 100)
-            send.sendHeadMotor(2, HEAD_Y_HIGH, 50)
+            send.sendHeadMotor(2, HEAD_Y_LOW, 50)
             time.sleep(0.01)
 
     def obs_find(self):
@@ -55,15 +55,15 @@ class United_Soccer:
             send.sendContinuousValue(1000, 0, 0, -(self.obs.center.x) // 40, 0)
 
     def catch_ball(self):
-        if self.ball.center.x < 125:
-            self.head_x += 8
-        if self.ball.center.x > 145:
-            self.head_x -= 8
+        if self.ball.center.x < 150:
+            self.head_x += 5
+        if self.ball.center.x > 170:
+            self.head_x -= 5
         if self.ball.center.y < 110:
-            self.head_y += 8
+            self.head_y += 5
         if self.ball.center.y > 130:
-            self.head_y -= 8
-        self.head_status = "Low"
+            self.head_y -= 5
+        self.head_status = "High"
         rospy.logdebug(f'color_x = {self.ball.center.x} , color_y = {self.ball.center.y}')
         rospy.loginfo(f'head_x = {self.head_x} , head_y = {self.head_y}')
         send.sendHeadMotor(1, self.head_x, 100)
@@ -94,7 +94,7 @@ class United_Soccer:
         if self.obs.get_target:
 
             if self.obs.edge_min.x < 200:
-                send.sendBodySector(111)
+                send.sendBodySector(2222)
                 time.sleep(5)
                 self.kick = True
                 rospy.loginfo('黃金右腳')
@@ -122,13 +122,13 @@ class United_Soccer:
                 if send.DIOValue == 25 and not self.preturn:
                     send.sendContinuousValue(3500, 0, 0, 0, 0)
                     time.sleep(15)
-                    while send.imu_value_Yaw < 90:
+                    while send.imu_value_Yaw < 80:
                         send.sendContinuousValue(0, 0, 0, 5, 0)
                     self.preturn = True
                 elif send.DIOValue == 27 and not self.preturn:
                     send.sendContinuousValue(3500, 0, 0, 0, 0)
                     time.sleep(15)
-                    while send.imu_value_Yaw > -90:
+                    while send.imu_value_Yaw > -80:
                         send.sendContinuousValue(0, 0, 0, -5, 0)        
                     self.preturn = True
             self.ball.update()
@@ -146,13 +146,21 @@ class United_Soccer:
             else:
                 if self.head_y <= 1305:
                     send.sendBodyAuto(0, 0, 0, 0, 1, 0)
-                    time.sleep(1)
+                    time.sleep(1.5)
                     self.attack_obs()
                     if not self.kick:
-                        send.sendBodySector(111)
-                        time.sleep(4)
                         self.head_x = 2048
-                        self.head_y = 1800
+                        self.head_y = 1305
+                        send.sendHeadMotor(1, self.head_x, 100)
+                        send.sendHeadMotor(2, self.head_y, 100)
+                        time.sleep(1)
+                        if self.ball.center.x > 155:
+                            send.sendBodySector(222)
+                        else:
+                            send.sendBodySector(111)
+                        time.sleep(5.5)
+                        self.head_y = 1400
+                        
                     self.init()
                 rospy.loginfo("goto_ball")
                 self.catch_ball()
@@ -169,7 +177,7 @@ class United_Soccer:
                 self.first_in = True
                 self.preturn = False
                 rospy.loginfo('stop')
-            # self.obs.update()
+            self.ball.update()
 
     def init(self):
         self.send_theta = 0
@@ -236,7 +244,7 @@ class ObjectInfo:
 
             
             # rospy.loginfo(self.edge_max.x )
-            # rospy.loginfo(self.edge_min.x )
+            rospy.loginfo(self.center.x )
             rospy.logdebug(abs(abs(self.edge_max.x - self.edge_min.x) - abs(self.edge_max.y - self.edge_min.y)))
             send.drawImageFunction(1, 1, self.edge_min.x, self.edge_max.x, self.edge_min.y, self.edge_max.y, 0, 0, 255)
         else:
